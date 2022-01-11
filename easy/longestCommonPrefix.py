c958568dd5641ed96326f233d5fd0a5a4c7ddedb
Shrenik Nambiar <shreniknambiar86@gmail.com>
2022-01-11 05:05:34 -0500

    optimized

--- easy/longestCommonPrefix.py
+++ easy/longestCommonPrefix.py
@@ -1,11 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         low = strs[0]
-        j=0
-        for i in range(0,len(strs)-1):
-            if(len(strs[i])<len(strs[i+1])):
-                low = strs[i]
-                
+        j=0     
         while j < len(strs):
             if strs[j].startswith(low):
                 j=j+1
