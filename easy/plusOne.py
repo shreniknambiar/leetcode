391ebe9e80aa42f15c9515c22c08fdcf05d38b31
shreniknambiar <shreniknambiar86@gmail.com>
2022-01-11 07:51:52 -0500

    lsb add

---
+++ easy/plusOne.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i=0
+        j=0
+        l=len(digits)-1
+        res=0
+        if digits[l] != 9:
+            digits[l] = digits[l]+1
+            return digits
+        else:
+            while i<len(digits):
+                dig = digits[l]
+                num = dig*(10**i)
+                res = res + num
+                i+=1
+                l-=1
+            res+=1
+            res_list = []
+            while j<res:
+                mod = res%10
+                res_list.insert(0,mod)
+                res=res//10
+            return res_list
